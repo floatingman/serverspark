82082eb2955eccf379152fd8a4b030209efc843b
Daniel Newman <dwnewman78@gmail.com>
2016-12-31 19:07:48 -0600

    intial commit for server ansible

---
+++ roles/browsers/files/tor-browser-en.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Launch the Tor Browser, verifying that PaX flags are
+# properly set for a grsec kernel.
+#
+###############################################################################
+
+BROWSER=~/.tor-browser-en/INSTALL/Browser/firefox
+TBB=/usr/bin/tor-browser-en
