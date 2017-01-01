7a9ebc7d36a4ac587bbe676eee651c226a76a167
Daniel Newman <dwnewman78@gmail.com>
2016-12-31 20:31:50 -0600

    removed unecessary roles

--- roles/browsers/files/tor-browser-en.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Launch the Tor Browser, verifying that PaX flags are
-# properly set for a grsec kernel.
-#
-###############################################################################
-
-BROWSER=~/.tor-browser-en/INSTALL/Browser/firefox
-TBB=/usr/bin/tor-browser-en
--- roles/calibre/files/firejail/calibre.profile
+++
@@ -1,3 +0,0 @@
-private-dev
-private-etc passwd,group,hostname,hosts,nsswitch.conf,resolv.conf,gtk-2.0,gtk-3.0,fonts,mime.types
-private-tmp
